03d01c9310e632f5bfddbff22796c492682c5f10
Lucas Martins <83207781+LucasSMartins@users.noreply.github.com>
2022-08-27 17:21:39 -0300

    foi criado um input e um button
     e ambos foram estilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import './App.css'
+import { Player } from './components/player'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -27,6 +28,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Player></Player>
+
+
     </div>
   )
 }
---
+++ src/containers/button.tsx
@@ -0,0 +1,6 @@
+import { styled } from "@stitches/react";
+
+
+export const Button = styled('button',{
+    backgroundColor:"green"
+}) 
